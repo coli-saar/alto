bfb2cfb9229b56e934980c1084fb32cfc4265068
Alexander Koller <akoller@gmail.com>
2010-12-25 23:11:30 +0100

    Implemented homomorphisms.

--- src/main/java/de/saar/penguin/irtg/hom/Homomorphism.java
+++ src/main/java/de/saar/penguin/irtg/hom/Homomorphism.java
@@ -6,9 +6,13 @@
 package de.saar.penguin.irtg.hom;
 
 import de.saar.basic.tree.Tree;
+import de.saar.basic.tree.TreeVisitor;
 import de.saar.chorus.term.Constant;
 import de.saar.chorus.term.Term;
+import de.saar.chorus.term.Variable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,7 +30,48 @@ public class Homomorphism {
         mappings.put(label, mapping);
     }
 
-    public Tree<Term> map(Tree<Term> tree) {
-        return null; // TODO implement
+    public Tree<Term> get(Constant label) {
+        return mappings.get(label);
+    }
+
+    public Tree<Term> apply(Tree<Term> tree) {
+        List<String> children = tree.getChildren(tree.getRoot());
+        List<Tree<Term>> subtrees = new ArrayList<Tree<Term>>();
+
+        for( String child : children ) {
+            subtrees.add(apply(tree.subtree(child)));
+        }
+
+        return construct(mappings.get(tree.getLabel(tree.getRoot())), subtrees);
+    }
+
+    private Tree<Term> construct(final Tree<Term> tree, final List<Tree<Term>> subtrees) {
+        final Tree<Term> ret = new Tree<Term>();
+
+        tree.dfs(new TreeVisitor<String,Void>() {
+            @Override
+            public String visit(String node, String parent) {
+                Term label = tree.getLabel(node);
+
+                if( label instanceof Variable ) {
+                    ret.insert(subtrees.get(getIndexForVariable(((Variable) label).getName())), parent);
+                    return null; // never used
+                } else {
+                    String newNode = ret.addNode(label, parent);
+                    return newNode;
+                }
+            }
+
+            @Override
+            public String getRootValue() {
+                return null;
+            }
+        });
+
+        return ret;
+    }
+
+    private static int getIndexForVariable(String varname) {
+        return Integer.parseInt(varname.substring(1)) - 1;
     }
 }
---
+++ src/test/groovy/de/saar/penguin/irtg/hom/HomomorphismTest.groovy
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.saar.penguin.irtg.hom
+
+
+import org.junit.*
+import java.util.*
+import java.io.*
+import static org.junit.Assert.*
+import de.saar.chorus.term.parser.*
+import de.saar.chorus.term.*
+import de.saar.basic.tree.*
+
+/**
+ *
+ * @author koller
+ */
+class HomomorphismTest {
+    @Test
+    public void testMap() {
+        Homomorphism h = hom(["f":"g(?1,h(?1))", "a":"k(b)"]);
+        Tree t = TermParser.parse("f(a)").toTree();
+        Term gold = TermParser.parse("g(k(b),h(k(b)))");
+        Tree rhs = h.apply(t);
+        Term rhsTerm = rhs.toTerm();
+
+        assertEquals(gold, rhsTerm);
+    }
+
+    @Test
+    public void testMapBinary() {
+        Homomorphism h = hom(["f":"g(?2,h(?1))", "a":"k(b)", "c":"l(e)"]);
+        Tree t = TermParser.parse("f(a,c)").toTree();
+        Term gold = TermParser.parse("g(l(e),h(k(b)))");
+        Tree rhs = h.apply(t);
+        Term rhsTerm = rhs.toTerm();
+
+        assertEquals(gold, rhsTerm);
+    }
+
+
+    private static Homomorphism hom(Map<String,String> mappings) {
+        Homomorphism ret = new Homomorphism();
+
+        mappings.each {
+            ret.add(new Constant(it.key), TermParser.parse(it.value).toTree());
+        }
+
+        return ret;
+    }
+}
+
