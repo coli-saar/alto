5c553a2aa87f28e5a2071d2eeed44643dc2bf9d2
Alexander Koller <akoller@gmail.com>
2010-12-25 19:55:42 +0100

    Added another test case.

--- src/test/groovy/de/saar/penguin/irtg/algebra/StringAlgebraTest.groovy
+++ src/test/groovy/de/saar/penguin/irtg/algebra/StringAlgebraTest.groovy
@@ -10,6 +10,8 @@ import java.util.*
 import java.io.*
 import de.saar.penguin.irtg.automata.*
 import static org.junit.Assert.*
+import de.saar.chorus.term.parser.*;
+import de.saar.basic.tree.*;
 
 /**
  *
@@ -26,6 +28,15 @@ class StringAlgebraTest {
         assertEquals(new HashSet([s(0,7)]), auto.getFinalStates());
         assertEquals([s(2,4)], auto.getParentStates(StringAlgebra.CONCAT, [s(2,3), s(3,4)]));
         assertEquals([], auto.getParentStates(StringAlgebra.CONCAT, [s(2,3), s(4,5)]));
+    }
+
+    @Test
+    public void testEvaluate() {
+        String string = "john watches the woman with the telescope";
+        Algebra algebra = new StringAlgebra();
+        Tree term = TermParser.parse("CONCAT(john,CONCAT(watches,CONCAT(the,CONCAT(woman,CONCAT(with,CONCAT(the,telescope))))))").toTree();
+
+        assertEquals(string, algebra.evaluate(term));
     }
 
     private StringAlgebra.Span s(int i, int k) {
