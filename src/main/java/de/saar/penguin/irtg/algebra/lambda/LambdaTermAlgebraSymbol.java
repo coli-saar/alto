c9cae0a7952e6b0e3cb6867b857193c86f97c70c
anna <anna@anna-desktop>
2011-11-08 23:54:16 +0100

    Added Class for LambdaTermAlgebraSymbol

---
+++ src/main/java/de/saar/penguin/irtg/algebra/lambda/LambdaTermAlgebraSymbol.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.saar.penguin.irtg.algebra.lambda;
+
+/**
+ *
+ * @author Anna Melzer
+ */
+public class LambdaTermAlgebraSymbol {
+
+        public static enum Type {
+
+        FUNCTOR, LTERM    };
+
+        public String type;
+        public LambdaTerm content;
+
+        public LambdaTermAlgebraSymbol(){
+
+
+        }
+
+        public static LambdaTermAlgebraSymbol functor(){
+            LambdaTermAlgebraSymbol ret = new LambdaTermAlgebraSymbol();
+            ret.type = "FUNCTOR";
+            return ret;
+
+        }
+
+        public static LambdaTermAlgebraSymbol lterm(LambdaTerm content){
+            LambdaTermAlgebraSymbol ret = new LambdaTermAlgebraSymbol();
+            ret.type = "LTERM";
+            ret.content = content;
+            return ret;
+        }
+
+        @Override
+        public String toString(){
+            if(this.type.equals("FUNCTOR")){
+                return "<";
+            }
+            else{
+                return content.getTree().toString();
+            }
+        }
+
+}
