fc8d1d395c0f6f4f4b38272048f0771b202798ac
danilo <danilo@deckelchen>
2012-11-20 14:40:23 +0100

    added tests for features and training

--- src/test/groovy/de/saar/penguin/irtg/maxent/MaximumEntropyIrtgTest.groovy
+++ src/test/groovy/de/saar/penguin/irtg/maxent/MaximumEntropyIrtgTest.groovy
@@ -26,7 +26,7 @@ class MaximumEntropyIrtgTest {
     public void testMaxentIrtgParsing() {
         InterpretedTreeAutomaton irtg = iparse(CFG_STR);
         assert irtg instanceof MaximumEntropyIrtg;
-        assertEquals( new HashSet(["f1","f2"]), irtg.getFeatureNames());
+        assertEquals( new ArrayList(["f1","f2"]), irtg.getFeatureNames());
 
         irtg.readWeights(new StringReader(WEIGHTS_STR));
         TreeAutomaton chart = irtg.parseFromReaders(i:new StringReader(SENTENCE_STR));
@@ -38,11 +38,42 @@ class MaximumEntropyIrtgTest {
         }
     }
     
+    @Test
+    public void testFeatures() {
+        FeatureFunction featureFunction = new AlignPPtoNFeature();
+        de.saar.basic.Pair parent1 = new de.saar.basic.Pair<String,String>("N","0-4");
+        de.saar.basic.Pair[] children = new de.saar.basic.Pair<String,String>[2];
+        children[0] = new de.saar.basic.Pair<String,String>("N","0-2");
+        children[1] = new de.saar.basic.Pair<String,String>("PP","2-4");
+        Rule r1 = new Rule<de.saar.basic.Pair<String,String>>(parent1, "r1", children, 0.0);
+        assert featureFunction.evaluate(r1) == 1.0, "feature weight shall be 1.0";
+
+        de.saar.basic.Pair parent2 = new de.saar.basic.Pair<String,String>("VP","0-4");
+        Rule r2= new Rule<de.saar.basic.Pair<String,String>>(parent2, "r2", children, 0.0);
+        assert featureFunction.evaluate(r2) == 0.0, "feature weight shall be 0.0";
+
+        children[1] = new de.saar.basic.Pair<String,String>("XX","0-0");
+        Rule r3= new Rule<de.saar.basic.Pair<String,String>>(parent1, "r2", children, 0.0);
+        assert featureFunction.evaluate(r3) == 0.0, "feature weight shall be 0.0";
+    }
+    
+    @Test
+    public void testMaxEntTraining() {
+        InterpretedTreeAutomaton irtg = iparse(CFG_STR);
+        assert irtg instanceof MaximumEntropyIrtg;
+        AnnotatedCorpus anCo = AnnotatedCorpus.readAnnotatedCorpus(new StringReader(TRAIN2_STR), irtg);
+        /*
+        irtg.train(anCo);
+        double[] fWeights = irtg.getFeatureWeights();
+        assertEquals( 0.0, fWeights[0]);
+        assertEquals( 1.0, fWeights[1]);
+        */
+    }
     
     private static final String CFG_STR = """
 interpretation i: de.saar.penguin.irtg.algebra.StringAlgebra
 feature f1: de.saar.penguin.irtg.maxent.StaticFeature
-feature f2: de.saar.penguin.irtg.maxent.DetFeature
+feature f2: de.saar.penguin.irtg.maxent.AlignPPtoNFeature
 r1(NP,VP) -> S!
   [i] *(?1,?2)
 r4(V,NP) -> VP 
@@ -73,6 +104,25 @@ f1 = 0.2
 f2 = 0.8""";
     private static final String SENTENCE_STR = "john watches the woman with the telescope";
 	
+    private static final String TRAIN1_STR = """
+i
+john watches the woman with the telescope
+r1(r7,r5( r4(r8, r2(r9,r10)), r6(r12, r2(r9,r11))))
+john watches the telescope with the telescope
+r1(r7,r5( r4(r8, r2(r9,r11)), r6(r12, r2(r9,r11))))
+john watches the telescope with the woman
+r1(r7,r5( r4(r8, r2(r9,r11)), r6(r12, r2(r9,r10))))
+    """;
+
+    private static final String TRAIN2_STR = """
+i
+john watches the woman with the telescope
+r1(r7,r4( r8, r2(r9,r3(r10, r6(r12, r2(r9,r11))))))
+john watches the telescope with the telescope
+r1(r7,r4( r8, r2(r9,r3(r11, r6(r12, r2(r9,r11))))))
+john watches the telescope with the woman
+r1(r7,r4( r8, r2(r9,r3(r11, r6(r12, r2(r9,r10))))))
+    """;
     
     private static InterpretedTreeAutomaton iparse(String s) {
         return IrtgParser.parse(new StringReader(s));
@@ -87,34 +137,5 @@ public class StaticFeature implements FeatureFunction {
 
     public double evaluate(Rule rule){
         return this.staticFeatureValue;
-    }
-}
-
-public class DetFeature implements FeatureFunction {
-    private static final double DEFAULT_VALUE = 0.5;
-
-    public double evaluate(Rule rule){
-        String label = rule.getLabel();
-        if(label.equals("r1")){
-            return 0.1;
-        }else if(label.equals("r2")){
-            return 0.2;
-        }else if(label.equals("r3")){
-            return 0.7;
-        }else if(label.equals("r4")){
-            return 0.3;
-        }else if(label.equals("r5")){
-            return 0.08;
-        }else if(label.equals("r6")){
-            return 0.24;
-        }else if(label.equals("r7")){
-            return 0.8;
-        }else if(label.equals("r8")){
-            return 0.54;
-        }else if(label.equals("r9")){
-            return 0.66;
-        }else{
-            return DEFAULT_VALUE;
-        }
     }
 }
