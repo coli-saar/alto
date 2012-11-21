18a21df5c3f6bf88c5476797826151d4231875e6
danilo <danilo@deckelchen>
2012-11-21 11:22:30 +0100

    moved function to super class

--- src/main/java/de/saar/penguin/irtg/maxent/FeatureFunction.java
+++ src/main/java/de/saar/penguin/irtg/maxent/FeatureFunction.java
@@ -5,11 +5,23 @@
 package de.saar.penguin.irtg.maxent;
 
 import de.saar.penguin.irtg.automata.Rule;
+import de.saar.basic.Pair;
 
 /**
  *
  * @author koller
  */
-public interface FeatureFunction<State> {
-    public double evaluate(Rule<State> object);
+public abstract class FeatureFunction<State> {
+    public abstract double evaluate(Rule<State> object);
+
+    protected String getLabelFor(Object state) {
+        if (state instanceof Pair) {
+            Pair s = (Pair) state;
+            if (s.left instanceof Pair) {
+                return this.getLabelFor((Pair) s.left);
+            }
+            return (String) s.left;
+        }
+        return (String) state;
+    }
 }
