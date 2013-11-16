82b0dc94cda4d6e621f42cb740f804702febff3c
Alexander Koller <akoller@gmail.com>
2013-11-16 11:43:18 +0100

    evaluation in GraphAlgebra works

--- src/main/java/de/up/ling/irtg/algebra/graph/GraphAlgebra.java
+++ src/main/java/de/up/ling/irtg/algebra/graph/GraphAlgebra.java
@@ -9,20 +9,51 @@ import de.up.ling.irtg.algebra.ParserException;
 import de.up.ling.irtg.automata.TreeAutomaton;
 import de.up.ling.irtg.signature.Signature;
 import de.up.ling.tree.Tree;
+import de.up.ling.tree.TreeVisitor;
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.swing.JComponent;
-import javax.swing.JFrame;
 
 /**
  *
  * @author koller
  */
 public class GraphAlgebra extends Algebra<LambdaGraph> {
-    
+    private Map<String,GraphCombiningOperation> operations;
+    private Signature signature;
 
+    public GraphAlgebra() {
+        operations = new HashMap<String, GraphCombiningOperation>();
+        signature = new Signature();
+    }
+    
+    protected GraphCombiningOperation op(String opDescription, int arity) throws ParseException {
+        GraphCombiningOperation ret = operations.get(opDescription);
+        
+        if( ret == null ) {
+            ret = IsiAmrParser.parseOperation(new StringReader(opDescription));
+            getSignature().addSymbol(opDescription, arity);
+            operations.put(opDescription, ret);
+        }
+        
+        return ret;
+    }
+    
     @Override
     public LambdaGraph evaluate(Tree<String> t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return t.dfs(new TreeVisitor<String, Void, LambdaGraph>() {
+            @Override
+            public LambdaGraph combine(Tree<String> node, List<LambdaGraph> childrenValues) {
+                try {
+                    GraphCombiningOperation op = op(node.getLabel(), childrenValues.size());
+                    return op.evaluate(childrenValues);
+                } catch (ParseException ex) {
+                    throw new IllegalArgumentException("Could not parse operation \"" + node.getLabel() + "\": " + ex.getMessage());
+                }
+            }           
+        });
     }
 
     @Override
@@ -41,7 +72,7 @@ public class GraphAlgebra extends Algebra<LambdaGraph> {
 
     @Override
     public Signature getSignature() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return signature;
     }
 
     @Override
---
+++ src/test/groovy/de/up/ling/irtg/algebra/graph/GraphAlgebraTest.groovy
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.up.ling.irtg.algebra.graph
+
+
+import org.junit.*
+import java.util.*
+import java.io.*
+import de.up.ling.irtg.automata.*
+import static org.junit.Assert.*
+import de.saar.chorus.term.parser.*;
+import de.up.ling.tree.*;
+import de.up.ling.irtg.algebra.*;
+import de.up.ling.irtg.hom.*;
+import static de.up.ling.irtg.util.TestingTools.*;
+import de.up.ling.irtg.*
+
+import org.junit.*
+import java.util.*
+import java.io.*
+import de.up.ling.irtg.automata.*
+import static org.junit.Assert.*
+import de.saar.chorus.term.parser.*;
+import de.up.ling.tree.*;
+import de.up.ling.irtg.algebra.*;
+import de.up.ling.irtg.hom.*;
+import static de.up.ling.irtg.util.TestingTools.*;
+import de.up.ling.irtg.*
+
+
+import org.jgrapht.*;
+import org.jgrapht.alg.*;
+import org.jgrapht.graph.*;
+
+import static Util.*;
+
+
+/**
+ *
+ * @author koller
+ */
+class GraphAlgebraTest {
+	@Test
+        public void testEvaluate() {
+            GraphAlgebra alg = new GraphAlgebra()
+            Tree<String> term = Tree.create("(w / want-01  :ARG0 (b)  :ARG1 (g)) + apply(?1, b) + apply(?2, g, b)",
+                                    [Tree.create("\\x (x / boy)"), Tree.create("\\g, b (g / go-01  :ARG0 (b))")]);
+                                
+            LambdaGraph result = alg.evaluate(term);
+            
+            assertIsomorphic(pg(IsiAmrParserTest.AMR1), result);
+        }
+}
+
