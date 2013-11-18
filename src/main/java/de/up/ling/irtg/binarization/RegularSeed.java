5ba69d7db51edc86390171af6c2354f0dc50fc9f
Alexander Koller <koller@ling.uni-potsdam.de>
2013-11-18 17:34:56 +0100

    caching regular seed automata to improve binarization performance

--- src/main/java/de/up/ling/irtg/binarization/RegularSeed.java
+++ src/main/java/de/up/ling/irtg/binarization/RegularSeed.java
@@ -6,7 +6,6 @@ package de.up.ling.irtg.binarization;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Iterators;
-import de.up.ling.irtg.algebra.Algebra;
 import de.up.ling.irtg.automata.ConcreteTreeAutomaton;
 import de.up.ling.irtg.automata.Rule;
 import de.up.ling.irtg.automata.TreeAutomaton;
@@ -14,7 +13,9 @@ import de.up.ling.irtg.signature.Signature;
 import de.up.ling.tree.Tree;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.ServiceLoader;
 import java.util.Set;
 
@@ -23,48 +24,58 @@ import java.util.Set;
  * @author koller
  */
 public abstract class RegularSeed {
+    private Map<String, TreeAutomaton> binarizationCache = new HashMap<String, TreeAutomaton>();
+
     /**
-     * Computes an automaton for all binarizations of a given symbol.
-     * If f is a k-place symbol, then each binarization is a term that
-     * contains each variable symbol ?1, ..., ?k exactly once.
-     * The intended use of these automata is that every term accepted
-     * by the automaton evaluates to the same function over some given
-     * algebra as the original k-place symbol (but expresses this function
-     * using symbols of lower arity).<p>
-     * 
+     * Computes an automaton for all binarizations of a given symbol. If f is a
+     * k-place symbol, then each binarization is a term that contains each
+     * variable symbol ?1, ..., ?k exactly once. The intended use of these
+     * automata is that every term accepted by the automaton evaluates to the
+     * same function over some given algebra as the original k-place symbol (but
+     * expresses this function using symbols of lower arity).<p>
+     *
      * The automaton returned by this method must satisfy a number of
      * requirements.
      * <ul>
      * <li> The automaton has exactly one final state.</li>
-     * <li> There is no state that expands both into a variable
-     * (i.e. there is a rule q -&gt; ?i) and into a terminal symbol
-     * (i.e. there is a rule q -&gt; f(q1,...,qn) where f is not
-     * of the form ?i).</li>
+     * <li> There is no state that expands both into a variable (i.e. there is a
+     * rule q -&gt; ?i) and into a terminal symbol (i.e. there is a rule q -&gt;
+     * f(q1,...,qn) where f is not of the form ?i).</li>
      * </ul>
-     * 
+     *
      * @param symbol
-     * @return 
+     * @return
      */
     public abstract TreeAutomaton<String> binarize(String symbol);
 
     /**
-     * Computes an automaton for all binarizations of a given term.
-     * This automaton is obtained from the binarizations of the individual
-     * symbols by tree automaton substitution. That is, the binarization
-     * of f(t1,...,tn) contains all the rules from the binarizations of f
-     * and all t1,...,tn. The states of these automata are renamed apart,
-     * and connected in the following way. If the binarization automaton for f
-     * contains a rule q -> ?i (i.e., the i-th variable), then all occurrences
-     * of q in the automaton are replaced by the final state of the binarization
-     * automaton for ti.
-     * 
+     * Computes an automaton for all binarizations of a given term. This
+     * automaton is obtained from the binarizations of the individual symbols by
+     * tree automaton substitution. That is, the binarization of f(t1,...,tn)
+     * contains all the rules from the binarizations of f and all t1,...,tn. The
+     * states of these automata are renamed apart, and connected in the
+     * following way. If the binarization automaton for f contains a rule q ->
+     * ?i (i.e., the i-th variable), then all occurrences of q in the automaton
+     * are replaced by the final state of the binarization automaton for ti.
+     *
      * @param term
-     * @return 
+     * @return
      */
     public TreeAutomaton<String> binarize(Tree<String> term) {
         ConcreteTreeAutomaton<String> ret = new ConcreteTreeAutomaton<String>();
         int finalState = binarize(term, ret, "q");
         ret.addFinalState(finalState);
+        return ret;
+    }
+    
+    private TreeAutomaton binarizeCached(String label) {
+        TreeAutomaton ret = binarizationCache.get(label);
+        
+        if( ret == null ) {
+            ret = binarize(label);
+            binarizationCache.put(label, ret);
+        }
+        
         return ret;
     }
 
@@ -80,8 +91,8 @@ public abstract class RegularSeed {
 
         // compute binarization automaton for the current node and compute map
         // that maps q to i iff q -> ?i is a rule
-        TreeAutomaton<?> autoHere = binarize(term.getLabel());
-        
+        TreeAutomaton<?> autoHere = binarizeCached(term.getLabel());
+
         Int2IntMap variableStates = findVariableStates(autoHere, arity);
 
         // copy rules of this automaton into ret, renaming states
@@ -130,16 +141,16 @@ public abstract class RegularSeed {
             Set<Rule> rules = automaton.getRulesBottomUp(varid, emptyChildren);
             assert rules.size() == 1 : "found " + rules.size() + " rules for " + varsym; // by assumption, see above
 
-            ret.put(rules.iterator().next().getParent(), i-1);
+            ret.put(rules.iterator().next().getParent(), i - 1);
         }
 
         return ret;
     }
-    
+
     /**
      * Returns an iterator over all subclasses of RegularSeed.
-     * 
-     * @return 
+     *
+     * @return
      */
     public static Iterator<Class> getAllRegularSeedClasses() {
         ServiceLoader<RegularSeed> rsLoader = ServiceLoader.load(RegularSeed.class);
@@ -150,4 +161,4 @@ public abstract class RegularSeed {
         });
     }
 }
-
+ 
