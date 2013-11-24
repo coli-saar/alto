7422f8f0b4feb9de18b8d3f2396ef487a5855a24
Alexander Koller <akoller@gmail.com>
2013-11-24 22:28:24 +0100

    tree algebra with explicit arities

--- src/main/java/de/up/ling/irtg/algebra/TreeAlgebra.java
+++ src/main/java/de/up/ling/irtg/algebra/TreeAlgebra.java
@@ -34,7 +34,6 @@ public class TreeAlgebra extends Algebra<Tree<String>> {
     @Override
     public TreeAutomaton decompose(Tree<String> value) {
         return new SingletonAutomaton(value);
-//        return new TreeDecomposingAutomaton(value);
     }
 
     @Override
@@ -47,108 +46,10 @@ public class TreeAlgebra extends Algebra<Tree<String>> {
         return new TreePanel(object);
     }
     
-    
-//
-//    private class TreeDecomposingAutomaton extends TreeAutomaton<String> {
-//        private Tree<Integer> tree;
-//        private final Set<Integer> labels;
-//        private ListMultimap<Integer, Integer> leafLabelsToStateIds;
-//
-//        public TreeDecomposingAutomaton(Tree<String> derivedTree) {
-//            super(TreeAlgebra.this.getSignature());
-//
-//            this.tree = TreeAlgebra.this.getSignature().addAllSymbols(derivedTree);
-//
-//            labels = new HashSet<Integer>();
-//            leafLabelsToStateIds = ArrayListMultimap.create();
-//
-//            collectStatesAndLabels(tree, "q");
-//            finalStates.add(addState("q"));
-//        }
-//
-//        @Override
-//        public Set<Rule> getRulesBottomUp(int label, int[] childStates) {
-//            Set<Rule> ret = new HashSet<Rule>();
-//
-//            if (childStates.length == 0) {
-//                for (int state : leafLabelsToStateIds.get(label)) {
-//                    Rule rule = createRule(state, label, childStates, 1);
-//                    storeRule(rule);
-//                    ret.add(rule);
-//                }
-//            } else {
-//                String firstChildState = getStateForId(childStates[0]);
-//                String potentialParent = firstChildState.substring(0, firstChildState.length() - 1);
-//                boolean correctChildren = true;
-//
-//                for (int i = 0; i < childStates.length; i++) {
-//                    if (! getStateForId(childStates[i]).equals(potentialParent + i)) {
-//                        correctChildren = false;
-//                    }
-//                }
-//
-//                if (correctChildren && tree.select(potentialParent, 1).getLabel().equals(label)) {
-//                    Rule rule = createRule(addState(potentialParent), label, childStates, 1);
-//                    storeRule(rule);
-//                    ret.add(rule);
-//                }
-//            }
-//
-//            return ret;
-//        }
-//
-//        @Override
-//        public Set<Rule> getRulesTopDown(int label, int parentState) {
-//            Set<Rule> ret = new HashSet<Rule>();
-//            String parentPath = getStateForId(parentState);
-//            Tree<Integer> t = tree.select(parentPath, 1);
-//
-//            if (t.getLabel().equals(label)) {
-//                int[] children = new int[t.getChildren().size()];
-//                for (int i = 0; i < t.getChildren().size(); i++) {
-//                    children[i] = addState(parentPath + i);
-//                }
-//
-//                Rule rule = createRule(parentState, label, children, 1);
-//                ret.add(rule);
-//                storeRule(rule);
-//            }
-//
-//
-//            return ret;
-//        }
-//
-//        private void collectStatesAndLabels(Tree<Integer> node, String state) {
-//            int stateId = addState(state);
-//            labels.add(node.getLabel());
-//
-//            if (node.getChildren().isEmpty()) {
-//                leafLabelsToStateIds.put(node.getLabel(), stateId);
-//            }
-//
-//            for (int i = 0; i < node.getChildren().size(); i++) {
-//                collectStatesAndLabels(node.getChildren().get(i), state + i);
-//            }
-//        }
-//
-//        @Override
-//        public boolean isBottomUpDeterministic() {
-//            return true;
-//        }
-//    }
-
     @Override
     public Tree<String> parseString(String representation) throws ParserException {
         Tree<String> ret = TreeParser.parse(representation);
         signature.addAllSymbols(ret);
         return ret;
     }
-    
-    /*  ** unused? **
-    public Tree<StringOrVariable> parseStringWithVariables(String representation) {
-        Tree<StringOrVariable> ret = TermParser.parse(representation).toTreeWithVariables();
-        signature.addAllConstants(ret);
-        return ret;
-    }
-    */
 }
---
+++ src/test/groovy/de/up/ling/irtg/algebra/TreeAlgebraTest.groovy
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.up.ling.irtg.algebra
+
+
+import org.junit.*
+import java.util.*
+import java.io.*
+import de.saar.basic.*
+import de.saar.chorus.term.parser.*
+import de.up.ling.tree.*
+import de.up.ling.irtg.*
+import de.up.ling.irtg.hom.*
+import de.up.ling.irtg.automata.*
+import de.up.ling.irtg.algebra.*
+import de.up.ling.irtg.signature.*
+import com.google.common.collect.Iterators;
+import static org.junit.Assert.*
+import static de.up.ling.irtg.util.TestingTools.*;
+import it.unimi.dsi.fastutil.ints.*;
+
+
+/**
+ *
+ * @author koller
+ */
+class TreeAlgebraTest {
+	@Test
+        public void testWithArities() {
+            TreeWithAritiesAlgebra a = new TreeWithAritiesAlgebra();
+            Tree t = a.parseString("f(a,g(b))")
+            Tree tw = pt("f_2(a_0, g_1(b_0))")
+            
+            TreeAutomaton auto = a.decompose(t)
+            assertEquals(new HashSet([tw]), auto.language())
+            
+            assertEquals(t, a.evaluate(tw))            
+        }
+        
+        @Test
+        public void testWithArities2() {
+            TreeWithAritiesAlgebra a = new TreeWithAritiesAlgebra();
+            Tree t = a.parseString("a(a,a(b))")
+            Tree tw = pt("a_2(a_0, a_1(b_0))")
+            
+            TreeAutomaton auto = a.decompose(t)
+            assertEquals(new HashSet([tw]), auto.language())
+            
+            assertEquals(t, a.evaluate(tw))            
+        }
+}
+
