386a81806f38e7075cd0ef602af160fa9ff8dcf8
Alexander Koller <akoller@gmail.com>
2015-06-08 19:42:22 +0200

    added remaining package javadocs

---
+++ src/main/java/de/up/ling/irtg/automata/index/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Index data structures for tree automata.
+ * 
+ */
+package de.up.ling.irtg.automata.index;
---
+++ src/main/java/de/up/ling/irtg/gui/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A graphical user interface (GUI) for Alto.
+ * 
+ * @see CondensedTreeAutomaton
+ * 
+ */
+package de.up.ling.irtg.gui;
