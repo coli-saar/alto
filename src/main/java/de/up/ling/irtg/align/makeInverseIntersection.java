604b34aa88f65a75d0f4cb03accd5bbddb1e81ba
Christoph Teichmann <Christoph.Teichmann.II@uni-potsdam.de>
2015-07-11 23:42:04 +0200

    added parameter for whether variables need to be matched, also started to work on an iterator for homomorphism construction

--- src/main/java/de/up/ling/irtg/align/makeInverseIntersection.java
+++ src/main/java/de/up/ling/irtg/align/makeInverseIntersection.java
@@ -9,6 +9,14 @@ import de.saar.basic.Pair;
 import de.up.ling.irtg.automata.InverseHomAutomaton;
 import de.up.ling.irtg.automata.TreeAutomaton;
 import de.up.ling.irtg.hom.Homomorphism;
+import de.up.ling.irtg.signature.Signature;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntIterator;
+import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -24,8 +32,10 @@ public class makeInverseIntersection {
      * @return 
      */
     public static Pair<TreeAutomaton,List<Homomorphism>> makeInverseIntersection(
-                                            List<Pair<TreeAutomaton,Set<String>>> input){
-        List<Homomorphism> mappings = makeMappings(input);
+                                            List<Pair<TreeAutomaton,Set<String>>> input,
+                                            boolean match){
+        Signature sig = new Signature();
+        List<Homomorphism> mappings = makeMappings(input, match, sig);
         
         TreeAutomaton ta = new InverseHomAutomaton(input.get(0).getLeft(), mappings.get(0));
         for(int i=1;i<mappings.size();++i)
@@ -42,7 +52,61 @@ public class makeInverseIntersection {
      * @param input
      * @return 
      */
-    private static List<Homomorphism> makeMappings(List<Pair<TreeAutomaton, Set<String>>> input) {
+    private static List<Homomorphism> makeMappings(List<Pair<TreeAutomaton, Set<String>>> input,
+                                                    boolean match, Signature sig) {
+        List<Homomorphism> l = new ArrayList<>();
+        for(int i=0;i<input.size();++i){
+            l.add(new Homomorphism(sig, input.get(i).getLeft().getSignature()));
+        }
+        
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }   
+    }
+    
+    /**
+     * 
+     */
+    private class IterationHelper implements Iterator<IntList>{
+
+        /**
+         * 
+         */
+        private final IntList output = new IntArrayList();
+        
+        /**
+         * 
+         */
+        private final List<Signature> input = new ArrayList<>();
+        
+        /**
+         * 
+         */
+        private final ObjectList<IntIterator> mainSource = new ObjectArrayList<>();
+        
+        /**
+         * 
+         * @param input 
+         */
+        public IterationHelper(List<Pair<TreeAutomaton, Set<String>>> input,
+                                    boolean mixInSpecials) {
+            for(int i=0;i<input.size();++i)
+            {
+                Signature s = input.get(i).getLeft().getSignature();
+                //TODO
+                
+            }
+            
+        }
+        
+        @Override
+        public boolean hasNext() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public IntList next() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
 }
