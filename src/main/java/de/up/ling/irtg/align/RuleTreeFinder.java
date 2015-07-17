7d4700dea53db2f839fcbb291ff59bde01d2af5d
Christoph Teichmann <christoph.teichmann.ii@uni-potsdam.de>
2015-07-17 09:22:47 -0700

    rule tree finder also works as planned, now only some comments are needed to finish the alignment stuff

--- src/main/java/de/up/ling/irtg/align/RuleTreeFinder.java
+++ src/main/java/de/up/ling/irtg/align/RuleTreeFinder.java
@@ -42,18 +42,7 @@ public class RuleTreeFinder {
      */
     public Pair<TreeAutomaton,AlignmentMapper> getRuleOptions(Pair<String,String> inputPair,
                                                                                 String algebraUsed){
-        AlignmentAlgebra alal = algebras.get(algebraUsed);
-        
-        Pair<RuleMarker, Pair<TreeAutomaton,TreeAutomaton>> alignments = alal.decomposePair(inputPair.getLeft(),
-                inputPair.getRight());
-        RuleMarker rlm = alignments.getLeft();
-        TreeAutomaton ta1 = alignments.getRight().getLeft();
-        TreeAutomaton ta2 = alignments.getRight().getRight();
-        
-        Pair<TreeAutomaton,Pair<Homomorphism,Homomorphism>> result = gen.makeInverseIntersection(ta1, ta2, rlm);
-        AlignmentMapper am = new AlignmentMapper(result.getRight().getLeft(), result.getRight().getRight(), rlm);
-        
-        return new Pair<>(result.getLeft(),am);
+        return this.possiblyReweigh(algebraUsed, inputPair, false);
     }
     
     /**
@@ -64,6 +53,17 @@ public class RuleTreeFinder {
      */
     public Pair<TreeAutomaton,AlignmentMapper> getRuleOptionsReweighted(Pair<String,String> inputPair,
                                                                                 String algebraUsed){
+        return possiblyReweigh(algebraUsed, inputPair, true);
+    }
+
+    /**
+     * 
+     * @param algebraUsed
+     * @param inputPair
+     * @param reweigh
+     * @return 
+     */
+    private Pair<TreeAutomaton, AlignmentMapper> possiblyReweigh(String algebraUsed, Pair<String, String> inputPair, boolean reweigh) {
         AlignmentAlgebra alal = algebras.get(algebraUsed);
         
         Pair<RuleMarker, Pair<TreeAutomaton,TreeAutomaton>> alignments = alal.decomposePair(inputPair.getLeft(),
@@ -72,23 +72,28 @@ public class RuleTreeFinder {
         TreeAutomaton ta1 = alignments.getRight().getLeft();
         TreeAutomaton ta2 = alignments.getRight().getRight();
         
+        MarkingPropagator mp = new MarkingPropagator();
+        ta1 = mp.introduce(ta1, rlm, 0);
+        ta2 = mp.introduce(ta2, rlm, 1);
+        
         Pair<TreeAutomaton,Pair<Homomorphism,Homomorphism>> result = gen.makeInverseIntersection(ta1, ta2, rlm);
-        AlignmentMapper am = new AlignmentMapper(result.getRight().getLeft(), result.getRight().getRight(), rlm);
         
         TreeAutomaton ta = result.getLeft();
         Homomorphism h1 = result.getRight().getLeft();
         Homomorphism h2 = result.getRight().getRight();
+        AlignmentMapper am = new AlignmentMapper(h1, h2, rlm);
         
-        Iterable<Rule> it = ta.getRuleIterable();
-        for(Rule r : it){
-            String label = r.getLabel(ta);
-            if(rlm.isFrontier(h1.get(label).getLabel()) || rlm.isFrontier(h2.get(label).getLabel())){
-                r.setWeight(1.1);
-            }else{
-                r.setWeight(0.1);
+        if(reweigh){
+            Iterable<Rule> it = ta.getRuleIterable();
+            for(Rule r : it){
+                String label = r.getLabel(ta);
+                if(rlm.isFrontier(h1.get(label).getLabel()) || rlm.isFrontier(h2.get(label).getLabel())){
+                    r.setWeight(1.1);
+                }else{
+                    r.setWeight(0.1);
+                }
             }
         }
-        
         return new Pair<>(result.getLeft(),am);
     }
 }
