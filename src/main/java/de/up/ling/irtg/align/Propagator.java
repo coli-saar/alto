5e2ad7faefa0ba6194bd74a9984777b8a1a2f368
Christoph Teichmann <Christoph.Teichmann.II@uni-potsdam.de>
2015-08-11 14:06:49 +0200

    wrote more code for finding and using rules.

--- src/main/java/de/up/ling/irtg/align/Propagator.java
+++ src/main/java/de/up/ling/irtg/align/Propagator.java
@@ -5,9 +5,10 @@
  */
 package de.up.ling.irtg.align;
 
+import de.up.ling.irtg.automata.ConcreteTreeAutomaton;
+import de.up.ling.irtg.automata.Rule;
 import de.up.ling.irtg.automata.TreeAutomaton;
 import de.up.ling.irtg.semiring.Semiring;
-import it.unimi.dsi.fastutil.ints.Int2DoubleMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.IntAVLTreeSet;
 import it.unimi.dsi.fastutil.ints.IntIterator;
@@ -31,7 +32,7 @@ public class Propagator {
      * @return 
      */
     public Int2ObjectMap<IntSet> propagate(TreeAutomaton input, StateAlignmentMarking alignments){
-         Int2ObjectMap<IntSet> map = input.evaluateInSemiring(vp, alignments);
+        Int2ObjectMap<IntSet> map = input.evaluateInSemiring(vp, alignments);
         return map;
     }
     
@@ -47,13 +48,19 @@ public class Propagator {
     
     /**
      * 
+     * @param <State>
      * @param input
      * @param propagatedAlignments
      * @return 
      */
-    public TreeAutomaton convert(TreeAutomaton input, Int2ObjectMap<IntSet> propagatedAlignments){
-        //TODO
-        return null;
+    public <State> TreeAutomaton<State> convert(TreeAutomaton<State> input, 
+            final Int2ObjectMap<IntSet> propagatedAlignments){
+        final ConcreteTreeAutomaton<State> output = new ConcreteTreeAutomaton<>(input.getSignature());
+        
+        Visitor visit = new Visitor(propagatedAlignments, output, input);
+        input.foreachStateInBottomUpOrder(visit);
+        
+        return output;
     }
     
     /**
@@ -106,4 +113,78 @@ public class Propagator {
             return ZERO;
         }
     };
+    
+    
+    /**
+     * A simple visitor used to construct the new automaton by copying the rules of the old and introducing some
+     * extra labels.
+     */
+    private class Visitor implements TreeAutomaton.BottomUpStateVisitor
+    {    
+        /**
+         * The variable sets we are aware of for the states.
+         */
+        private final Int2ObjectMap<IntSet> vars;
+        
+        /**
+         * the automaton we are constructing.
+         */
+        private final ConcreteTreeAutomaton goal;
+        
+        /**
+         * the original with which we started.
+         */
+        private final TreeAutomaton original;
+        
+
+        /**
+         * Construct a new instance.
+         * 
+         */
+        public Visitor(Int2ObjectMap<IntSet> vars, ConcreteTreeAutomaton goal, TreeAutomaton original) {
+            this.vars = vars;
+            this.goal = goal;
+            this.original = original;
+        }
+        
+        
+        @Override
+        public void visit(int state, Iterable<Rule> rulesTopDown) {
+            Object st = this.original.getStateForId(state);
+            String loopLabel = HomomorphismManager.VARIABLE_PREFIX+"_"+this.vars.get(state).toString();
+            // here we add a loop for the alignments
+            this.goal.addRule(this.goal.createRule(st, loopLabel, new Object[] {st}));
+            
+            if(original.getFinalStates().contains(state))
+            {
+                this.goal.addFinalState(this.goal.getIdForState(st));
+            }
+            
+            for(Rule r : rulesTopDown)
+            {
+                Object[] arr = makeCopy(r.getChildren());
+                String label = r.getLabel(original);
+                double weight = r.getWeight();
+                
+                this.goal.addRule(goal.createRule(st, label, arr, weight));
+            }
+        }
+
+        /**
+         * Creates a copy of a rules child states
+         * 
+         * @param children
+         * @return 
+         */
+        private Object[] makeCopy(int[] children) {
+            Object[] obs = new Object[children.length];
+            
+            for (int i = 0; i < children.length; i++) {
+                obs[i] = this.original.getStateForId(children[i]);
+            }
+            
+            return obs;
+        }
+        
+    }
 }
