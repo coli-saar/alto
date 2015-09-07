d1e15536d8404f7c936de49931e505b64029e215
Christoph Teichmann <Christoph.Teichmann.II@uni-potsdam.de>
2015-09-07 17:22:09 +0200

    Adds Empty SpanAlignmentMarking Implementation.
    Sometimes we might want to assign no markings to any state, for this case the Empty class in alignment_marking will always return an empty set and do it slightly faster than an empty SpecifiedAligner

---
+++ src/main/java/de/up/ling/irtg/align/alignment_marking/Empty.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.up.ling.irtg.align.alignment_marking;
+
+import de.up.ling.irtg.align.StateAlignmentMarking;
+import de.up.ling.irtg.automata.TreeAutomaton;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+
+/**
+ *
+ * @author christoph_teichmann
+ */
+public class Empty<State> extends StateAlignmentMarking {
+    /**
+     * 
+     */
+    private final static IntSet EMPTY = new IntOpenHashSet();
+    
+    /**
+     * 
+     * @param reference 
+     */
+    public Empty(TreeAutomaton reference) {
+        super(reference);
+    }
+
+    @Override
+    public IntSet getAlignmentMarkers(Object state) {
+        return EMPTY;
+    }
+}
