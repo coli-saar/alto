f43e6a98f1bc1d54566268799c87d390bb168867
Christoph Teichmann <Christoph.Teichmann.II@uni-potsdam.de>
2015-09-08 18:39:35 +0200

    Moves the HillClimbing code to DividedSampling.
    It makes more sense to attempt to sample from both sides than to attempt to find a single optimal tree. Therefore the learning coding was continued in that branch.

--- src/main/java/de/up/ling/irtg/align/find_rules/DividedSampling.java
+++ src/main/java/de/up/ling/irtg/align/find_rules/DividedSampling.java
@@ -5,19 +5,22 @@
  */
 package de.up.ling.irtg.align.find_rules;
 
-import de.saar.basic.Pair;
 import de.up.ling.irtg.InterpretedTreeAutomaton;
+import de.up.ling.irtg.align.HomomorphismManager;
+import de.up.ling.irtg.align.Propagator;
 import de.up.ling.irtg.align.StateAlignmentMarking;
-import de.up.ling.irtg.align.alignment_marking.SpanAligner;
 import de.up.ling.irtg.automata.TreeAutomaton;
 import de.up.ling.irtg.signature.Signature;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.IntSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
  * @author christoph_teichmann
  */
-public class HillClimbing {
+public class DividedSampling {
     
     /**
      * 
@@ -25,7 +28,7 @@ public class HillClimbing {
     private final int threads;
     
     
-    public HillClimbing(int threads){
+    public DividedSampling(int threads){
         this.threads = threads;
     }
     
@@ -33,8 +36,21 @@ public class HillClimbing {
     public InterpretedTreeAutomaton findRules(List<TreeAutomaton> left, List<TreeAutomaton> right,
                                             List<StateAlignmentMarking> leftAlign,
                                             List<StateAlignmentMarking> rightAlign, Signature leftSig,
-                                            Signature rightSig){
+                                            Signature rightSig, Propagator prop){
+        Signature shared = new Signature();
+        List<HomomorphismManager> connections = new ArrayList<>();
         
+        for(int i=0;i<left.size() && i<right.size();++i){
+            TreeAutomaton t1 = left.get(i);
+            TreeAutomaton t2 = right.get(i);
+            
+            Int2ObjectMap<IntSet> setL = prop.propagate(t1, leftAlign.get(i));
+            Int2ObjectMap<IntSet> setR = prop.propagate(t2, rightAlign.get(i));
+            
+            
+            
+            
+        }
         
         
         //TODO
