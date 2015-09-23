e5b94cd7dbbc14467ace460d7350e18097d74e11
Christoph Teichmann <Christoph.Teichmann.II@uni-potsdam.de>
2015-09-23 13:22:31 +0200

    Corrects imports.
    This removes an unneeded import.

--- src/main/java/de/up/ling/irtg/align/pruning/StringLeftOrRight.java
+++ src/main/java/de/up/ling/irtg/align/pruning/StringLeftOrRight.java
@@ -8,7 +8,6 @@ package de.up.ling.irtg.align.pruning;
 import de.up.ling.irtg.algebra.StringAlgebra.Span;
 import de.up.ling.irtg.align.Pruner;
 import de.up.ling.irtg.align.StateAlignmentMarking;
-import de.up.ling.irtg.automata.ConcreteTreeAutomaton;
 import de.up.ling.irtg.automata.FromRuleTreesAutomaton;
 import de.up.ling.irtg.automata.Rule;
 import de.up.ling.irtg.automata.TreeAutomaton;
