ea28f1633393d3d8c6be008569f271bc0e04df22
Christoph Teichmann <Christoph.Teichmann.II@uni-potsdam.de>
2015-10-19 20:17:25 +0200

    Adds work on SampleBenign.
    This adds some comments and also scales the adaption by the effective sample size.

--- src/main/java/de/up/ling/irtg/align/find_rules/sampling/SampleBenign.java
+++ src/main/java/de/up/ling/irtg/align/find_rules/sampling/SampleBenign.java
@@ -25,23 +25,34 @@ import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.commons.math3.random.Well44497a;
 
 /**
- *
+ * This class uses adaptive iterated importance sampling in order to draw a
+ * sample of rule trees from a given automaton.
+ * 
+ * The automaton MUST be well behaved: it must have at least one final states
+ * and from any state reachable top down from the final states, there must be
+ * a sequence of rules that terminates. It is also assumed that the automaton
+ * has no cycles. The sampler actually draws multiple samples and attempts to
+ * improve the proposal distribution from each intermediate sample.
+ * 
  * @author christoph_teichmann
  */
 public abstract class SampleBenign {
     
     /**
-     * 
+     * The initial smoothing for the rules that can be used for expanding a
+     * state.
      */
     private final double smooth;
     
     /**
-     * 
+     * The automaton being sampled.
      */
     private TreeAutomaton benign;
     
     /**
-     * 
+     * The sums of weights for each state, these are computed again for every
+     * round of sampling, as they may change during round, basically this is
+     * 'just' smoothing.
      */
     private final Int2DoubleMap stateNormalizers = new Int2DoubleOpenHashMap();
     
@@ -51,31 +62,38 @@ public abstract class SampleBenign {
     private final Int2DoubleOpenHashMap finalStateCounts = new Int2DoubleOpenHashMap();
 
     /**
+     * Counts the number of times a rule was seen during the adaption rules.
      * 
+     * Note that these counts may be fractional. When we have n effective samples
+     * and an observation gets a portion of d percent of that sample, then
+     * the rule counts for that observation are increased by d*n.
      */
     private final Object2DoubleOpenHashMap<Rule> ruleCounts = new Object2DoubleOpenHashMap<>();
     
     /**
-     * 
+     * Cache for the rules used to (substantially) speed up the sampling process.
      */
     private final Int2ObjectMap<List<Rule>> options = new Int2ObjectOpenHashMap<>();
     
     /**
-     * 
+     * Used in order to turn log weights into a CWF and for resampling.
      */
     private final ArraySampler arrSamp;
     
     /**
-     * 
+     * A random number generator used during sampling.
      */
     private final RandomGenerator rg;
     
     /**
-     * 
+     * The sum of the weights for final states, which is cached within a single
+     * sampling round.
      */
     private double finalStateSum = -1.0;
     
     /**
+     * Create a new instance with a random seed based on the time and with the
+     * given smoothing for the initial rule weights.
      * 
      * @param smooth
      */
@@ -87,6 +105,9 @@ public abstract class SampleBenign {
     }
     
     /**
+     * Creates a new instance with the given smoothing for initial rule weights
+     * and which is based on the given seed for the random number generation
+     * (intended to improve reproducibility).
      * 
      * @param smooth 
      * @param seed 
@@ -100,14 +121,24 @@ public abstract class SampleBenign {
     }
     
     /**
+     * Changes the given automaton.
+     * 
+     * When the automaton is changed, then all cached information is changed.
+     * It is assumed that once an automaton has been set its rules will not
+     * change. Otherwise there would be a problem with caching. 
      * 
      * @param to 
      */
     public void setAutomaton(TreeAutomaton to){
         this.benign = to;
+        this.options.clear();
     }
     
     /**
+     * Abstract method that returns a weight for a rule.
+     * 
+     * These weights will be summed up and then normalized
+     * to give a probability.
      * 
      * @param r
      * @return 
@@ -115,6 +146,8 @@ public abstract class SampleBenign {
     protected abstract double makeRuleWeight(Rule r);
     
     /**
+     * Returns the fractional count of the given state over all adaption rounds
+     * with the smoothing value added.
      * 
      * @param state
      * @return 
@@ -124,6 +157,7 @@ public abstract class SampleBenign {
     }
     
     /**
+     * Returns the raw fractional count of the given state without smoothing.
      * 
      * @param state
      * @return 
@@ -133,6 +167,8 @@ public abstract class SampleBenign {
     }
     
     /**
+     * Returns the fractional count of how often the rule was seen during
+     * adaption, without any smoothing.
      * 
      * @param rule
      * @return 
@@ -142,6 +178,8 @@ public abstract class SampleBenign {
     }
     
     /**
+     * Returns the fractional count of how often the rule was seen during adaption
+     * with smoothing added.
      * 
      * @param rule
      * @return 
@@ -151,37 +189,66 @@ public abstract class SampleBenign {
     }
     
     /**
+     * Starts a new round of sampling that will return an (unweighted) sample
+     * as a result - the automaton that is being sampled is the one from the
+     * last call to setAutomaton.
+     * 
+     * The counts of how often all rules and states have been seen during adaption
+     * will be reset, but rules are kept in a cache between calls, so changes
+     * in the automaton after the first round of getSample will be
+     * ignored, unless we hit a state that has not been cached.
+     * 
+     * Note that there will be a null pointer exception, if setAutomaton has not
+     * been called before (or was called with a null argument).
      * 
      * @param config
      * @return 
      */
     public List<Tree<Rule>> getSample(Configuration config){
+        if(this.benign == null){
+            throw new NullPointerException("The automaton to be sampled must"
+                    + "be defined with setAutomaton before sampling starts");
+        }
+        
+        // used to store the intermediate samples and their weights
         List<Tree<Rule>> sample = new ObjectArrayList<>();
         DoubleList weights = new DoubleArrayList();
-        List<Tree<Rule>> resample = new ObjectArrayList<>();
-        this.options.clear();
         
+        // here we do a number of adaption steps and take the actual sample
+        // from the last round
         for(int round=0;round<config.rounds;++round){
+            // we reset the adapted sums for each state
             this.finalStateSum = Double.NEGATIVE_INFINITY;
             this.stateNormalizers.clear();
             
+            // we clear the sample (in consecutive rounds, the adaption will
+            // already have taken place at this point)
             sample.clear();
             weights.clear();
             
+            // then find out how many samples we are supposed to generate
             int numberOfSamples = config.sampleSize.apply(round+1);
+            
+            // generate them, with the inverse proposal probabilist added to
+            // weights - as logs
             for(int samp=0;samp<numberOfSamples;++samp){
                 addSample(sample,weights);
             }
             
+            // add the actual target weights - as logs
             addTargetWeight(sample,weights,config);
             
+            // normalize the weights and turn them into a CDF
             this.arrSamp.turnIntoCWF(weights);
+            double effectiveSampleSize = makeESSFromCWF(weights);
             
             for(int i=0;i<weights.size();++i){
-                this.adapt(sample.get(i), weights.get(i) - (i > 0 ? weights.get(i-1) : 0.0));
+                this.adapt(sample.get(i),
+                      effectiveSampleSize*(weights.get(i) - (i > 0 ? weights.get(i-1) : 0.0)));
             }
         }
         
+        List<Tree<Rule>> resample = new ObjectArrayList<>();
         resample.clear();
         int numberOfSamples = config.sampleSize.apply(0);
         for(int samp=0;samp<numberOfSamples;++samp){
@@ -333,6 +400,27 @@ public abstract class SampleBenign {
         for(int i=0;i<t.getChildren().size();++i){
             adaptRules(t.getChildren().get(i),amount);
         }
+    }
+
+    /**
+     * 
+     * @param weights
+     * @return 
+     */
+    private double makeESSFromCWF(DoubleList weights) {
+        if(weights.size() <= 0){
+            return 0.0;
+        }
+        
+        double val = weights.get(0);
+        double sum = val*val;
+        for(int i=1;i<weights.size();++i){
+            val = weights.get(i)-weights.get(i-1);
+            
+            sum += val*val;
+        }
+        
+        return 1.0 / sum;
     }
     
     /**
