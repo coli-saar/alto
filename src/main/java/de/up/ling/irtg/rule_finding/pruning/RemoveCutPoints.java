4cc5db378ec2bbda3a2347a2d2104d1a4b2cdbbf
Christoph Teichmann <Christoph.Teichmann.II@uni-potsdam.de>
2016-03-05 19:00:23 +0100

    Removed a bug in Cut point reduction.
    This removes a but that would remove the top most variable.

--- src/main/java/de/up/ling/irtg/rule_finding/pruning/RemoveCutPoints.java
+++ src/main/java/de/up/ling/irtg/rule_finding/pruning/RemoveCutPoints.java
@@ -25,102 +25,105 @@ import java.util.function.Consumer;
  * @author christoph_teichmann
  */
 public class RemoveCutPoints {
+
     /**
-     * 
+     *
      * @param base
      * @param allowedStates
      * @param useRight
-     * @return 
+     * @return
      */
     public static InterpretedTreeAutomaton removeCutPoints(InterpretedTreeAutomaton base, Set<String> allowedStates,
             boolean useRight) {
-       TreeAutomaton basis = base.getAutomaton(); 
-       TreeAutomaton reduce = restrictedCopy(basis,allowedStates,useRight);
-       
-       InterpretedTreeAutomaton result = new InterpretedTreeAutomaton(reduce);
-       result.addAllInterpretations(base.getInterpretations());
-       
-       return result; 
+        TreeAutomaton basis = base.getAutomaton();
+        TreeAutomaton reduce = restrictedCopy(basis, allowedStates, useRight);
+
+        InterpretedTreeAutomaton result = new InterpretedTreeAutomaton(reduce);
+        result.addAllInterpretations(base.getInterpretations());
+
+        return result;
     }
-    
+
     /**
-     * 
+     *
      * @param in
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
     public static Set<String> makeRelevantSet(InputStream in) throws IOException {
         Set<String> allowed = new HashSet<>();
-        
-        try(BufferedReader input = new BufferedReader(new InputStreamReader(in))) {
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(in))) {
             String line;
-            
-            while((line = input.readLine()) != null) {
+
+            while ((line = input.readLine()) != null) {
                 line = line.trim();
-                if(line.isEmpty()) {
+                if (line.isEmpty()) {
                     continue;
                 }
-                
+
                 allowed.add(line);
             }
         }
-        
+
         return allowed;
     }
 
     /**
-     * 
+     *
      * @param basis
      * @param allowedStates
-     * @return 
+     * @return
      */
     private static TreeAutomaton restrictedCopy(TreeAutomaton basis, Set<String> allowedStates, boolean useRight) {
         ConcreteTreeAutomaton cta = new ConcreteTreeAutomaton(basis.getSignature());
         IntIterator iit = basis.getAllStates().iterator();
-        
-        while(iit.hasNext()) {
+
+        while (iit.hasNext()) {
             int parent = iit.nextInt();
             Object pState = basis.getStateForId(parent);
-            
+
             int newState = cta.addState(pState);
-            if(basis.getFinalStates().contains(parent)) {
+            if (basis.getFinalStates().contains(parent)) {
                 cta.addFinalState(newState);
             }
-            
+
             Consumer<Rule> con = (Rule rule) -> {
                 String label = basis.getSignature().resolveSymbolId(rule.getLabel());
-                
+
                 //check addmissible
-                if(Variables.isVariable(label)) {
+                if (Variables.isVariable(label)) {
                     String content = Variables.getInformation(label);
                     int pos = content.indexOf(HomomorphismManager.FINAL_VARIABLE_STATE_DELIMITER);
                     
-                    String relevant;
-                    if(useRight) {
-                        relevant = content.substring(pos+HomomorphismManager.FINAL_VARIABLE_STATE_DELIMITER.length());
-                    } else {
-                        relevant = content.substring(0,pos);
-                    }
-                    relevant = relevant.trim();
-                    
-                    //if not, stop here and go to next rule
-                    if(!allowedStates.contains(relevant)) {
-                        return;
+                    // make sure we are not dealing with e.g. the top Variable
+                    if (pos >= 0) {
+                        String relevant;
+                        if (useRight) {
+                            relevant = content.substring(pos + HomomorphismManager.FINAL_VARIABLE_STATE_DELIMITER.length());
+                        } else {
+                            relevant = content.substring(0, pos);
+                        }
+                        relevant = relevant.trim();
+
+                        //if not, stop here and go to next rule
+                        if (!allowedStates.contains(relevant)) {
+                            return;
+                        }
                     }
                 }
-                
+
                 Object[] children = new Object[rule.getArity()];
-                for(int i=0;i<rule.getArity();++i) {
+                for (int i = 0; i < rule.getArity(); ++i) {
                     children[i] = basis.getStateForId(rule.getChildren()[i]);
                 }
-                
+
                 cta.addRule(cta.createRule(pState, label, children));
             };
-            
+
             basis.foreachRuleTopDown(parent, con);
         }
-        
-        
+
         return cta;
     }
 }
