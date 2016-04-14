d625a52ea252a2db96a6e1b81709903d6ce828b4
Christoph Teichmann <christoph.teichmann.ii@uni-potsdam.de>
2016-04-14 15:51:06 -0700

    Create Method for obtaining all rules.
    This uses the existing tools for getting all rules and creates a new folder with irtgs that represent these rules.

--- src/main/java/de/up/ling/irtg/script/grammar_learning/ShowRules.java
+++ src/main/java/de/up/ling/irtg/script/grammar_learning/ShowRules.java
@@ -6,8 +6,9 @@
 package de.up.ling.irtg.script.grammar_learning;
 
 import de.up.ling.irtg.InterpretedTreeAutomaton;
+import de.up.ling.irtg.automata.TreeAutomaton;
 import de.up.ling.irtg.codec.IrtgInputCodec;
-import de.up.ling.irtg.rule_finding.nonterminals.ReplaceNonterminal;
+import de.up.ling.irtg.rule_finding.learning.GetAllRules;
 import de.up.ling.tree.ParseException;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -19,9 +20,9 @@ import java.util.Properties;
 
 /**
  *
- * @author christoph_teichmann
+ * @author teichmann
  */
-public class CreateNonterminalsFromInputFiles {
+public class ShowRules {
     /**
      * 
      * @param args
@@ -35,61 +36,30 @@ public class CreateNonterminalsFromInputFiles {
         
         String jointFolder = props.getProperty("jointTreeFolder");
         String outTreeFolder = props.getProperty("outputFolder");
-        String firstFolder = props.getProperty("firstStateMapFolder");
-        String secondFolder = props.getProperty("secondStateMapFolder");
-        String rootLabel = props.getProperty("rootLabel");
-        String defAult = props.getProperty("defaultLabel");
-        
-        
+    
         File jF = new File(jointFolder);
         File[] toDo = jF.listFiles();
         
         File outs = new File(outTreeFolder);
         outs.mkdirs();
         
-        File first = new File(firstFolder);
-        File second = new File(secondFolder);
-        
         IrtgInputCodec iic = new IrtgInputCodec();
+        
         for(int i=0;i<toDo.length;++i) {
             File current = toDo[i];
             
             InputStream jT = new FileInputStream(current);
-            
-            File fFile = makeFile(first, current);
-            
-            InputStream firstIn = new FileInputStream(fFile);
-            
-            File sFile = makeFile(second, current);
-            
-            InputStream secondIn = new FileInputStream(sFile);
-            
-            ReplaceNonterminal rn = new ReplaceNonterminal(firstIn, secondIn, rootLabel);
-            
             InterpretedTreeAutomaton ita = iic.read(jT);
             
-            ita = rn.introduceNonterminals(ita, defAult);
+            TreeAutomaton<String> t = GetAllRules.getAllRules(ita.getAutomaton());
+            
+            InterpretedTreeAutomaton qita = new InterpretedTreeAutomaton(t);
+            qita.addAllInterpretations(ita.getInterpretations());
             
             String name = outs.getAbsolutePath()+File.separator+current.getName();
-            try(BufferedWriter bw = new BufferedWriter(new FileWriter(name))) {
-                bw.write(ita.toString());
+             try(BufferedWriter bw = new BufferedWriter(new FileWriter(name))) {
+                bw.write(qita.toString());
             }
         }
-    }
-
-    /**
-     * 
-     * @param folder
-     * @param current
-     * @return 
-     */
-    private static File makeFile(File folder, File current) {
-        String nF = folder.getAbsolutePath()+File.separator+current.getName();
-        File fFile = new File(nF);
-        if(!fFile.exists()) {
-            nF = folder.getAbsolutePath()+File.separator+current.getName().replaceAll(".irtg$", "");
-            fFile = new File(nF);
-        }
-        return fFile;
     }
 }
