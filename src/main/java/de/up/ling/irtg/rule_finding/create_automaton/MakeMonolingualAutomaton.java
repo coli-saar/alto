845e8e936573dec22c7eff9ebe031898e12aed68
Christoph Teichmann <christoph.teichmann.ii@uni-potsdam.de>
2016-05-23 12:29:41 -0700

    Added simple tool to make all subtrees for a given automaton.
    Simply add X's

---
+++ src/main/java/de/up/ling/irtg/rule_finding/create_automaton/MakeMonolingualAutomaton.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.up.ling.irtg.rule_finding.create_automaton;
+
+import de.saar.basic.Pair;
+import de.up.ling.irtg.automata.ConcreteTreeAutomaton;
+import de.up.ling.irtg.automata.Rule;
+import de.up.ling.irtg.automata.TreeAutomaton;
+import de.up.ling.irtg.rule_finding.Variables;
+import de.up.ling.irtg.rule_finding.pruning.IntersectionPruner;
+import de.up.ling.irtg.rule_finding.pruning.Pruner;
+import de.up.ling.irtg.rule_finding.pruning.intersection.IntersectionOptions;
+import it.unimi.dsi.fastutil.ints.IntIterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+/**
+ *
+ * @author teichmann
+ */
+public class MakeMonolingualAutomaton {
+    
+    /**
+     * 
+     * @param <Type>
+     * @param ta
+     * @param nonterminals
+     * @param root
+     * @return 
+     */
+    public <Type> TreeAutomaton introduce(TreeAutomaton<Type> ta, BiFunction<TreeAutomaton<Type>,Type,String> nonterminals, Object root) {
+        ConcreteTreeAutomaton<Object> withIntroduction = new ConcreteTreeAutomaton<>();
+        
+        IntIterator states = ta.getAllStates().iterator();
+        while(states.hasNext()) {
+            int state = states.nextInt();
+            
+            Type parent = ta.getStateForId(state);
+            Pair<Type,Boolean> notSeen = new Pair<>(parent,false);
+            Pair<Type,Boolean> seen = new Pair<>(parent,true);
+            
+            String label = nonterminals.apply(ta, parent);
+            
+            List<Object> children = new ArrayList<>();
+            children.add(seen);
+            
+            withIntroduction.addRule(withIntroduction.createRule(notSeen, Variables.createVariable(label), children));
+            
+            Iterable<Rule> rules = ta.getRulesTopDown(state);
+            for(Rule r : rules) {
+                children.clear();
+                
+                String terminal = ta.getSignature().resolveSymbolId(r.getLabel());
+                for(int i=0;i<r.getArity();++i) {
+                    children.add(new Pair<>(ta.getStateForId(r.getChildren()[i]),false));
+                }
+                
+                withIntroduction.addRule(withIntroduction.createRule(notSeen, terminal, children));
+                withIntroduction.addRule(withIntroduction.createRule(seen, terminal, children));
+            }
+        }
+               
+        int finalState = withIntroduction.addState(root);
+        withIntroduction.addFinalState(finalState);
+        IntIterator fins = ta.getFinalStates().iterator();
+        
+        while(fins.hasNext()) {
+            Type t = ta.getStateForId(fins.nextInt());
+            String label = nonterminals.apply(ta, t);
+            label = Variables.createVariable(label);
+            
+            withIntroduction.addRule(withIntroduction.createRule(root, label, new Object[] {new Pair<>(t,true)}));
+        }
+        
+        return withIntroduction;
+    }
+}
