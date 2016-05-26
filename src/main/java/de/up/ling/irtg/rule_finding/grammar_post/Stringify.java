7c11dc5ab06515809adcff09e425e8dcab35fb50
Christoph Teichmann <christoph.teichmann.ii@uni-potsdam.de>
2016-05-26 13:40:14 -0700

    Make Stringify usable.
    The class now has a test and there is a simple script to use it.

--- src/main/java/de/up/ling/irtg/rule_finding/grammar_post/Stringify.java
+++ src/main/java/de/up/ling/irtg/rule_finding/grammar_post/Stringify.java
@@ -30,7 +30,7 @@ public class Stringify {
         Signature source = orig.getSourceSignature();
         Homomorphism hom = new Homomorphism(source, sal.getSignature());
         
-        for(int symbol=1;symbol < source.getMaxSymbolId();++symbol) {
+        for(int symbol=1;symbol <= source.getMaxSymbolId();++symbol) {
             String s = source.resolveSymbolId(symbol);
                         if(orig.get(symbol) != null) {
                 hom.add(s, treeRuleToStringRule(orig.get(s)));
@@ -50,6 +50,10 @@ public class Stringify {
     public static Tree<String> treeRuleToStringRule(Tree<String> homomorphicImage) {
         String label = homomorphicImage.getLabel();
         
+        if(homomorphicImage.getChildren().size() == 1) {
+            return treeRuleToStringRule(homomorphicImage.getChildren().get(0));
+        }
+        
         if(homomorphicImage.getChildren().isEmpty()) {
             if(label.equals(StringAlgebra.CONCAT)) {
                 return Tree.create(StringAlgebra.SPECIAL_STAR);
@@ -58,11 +62,19 @@ public class Stringify {
             }
         }
         
-        Tree<String>[] children = new Tree[homomorphicImage.getChildren().size()];
-        for(int i=0;i<homomorphicImage.getChildren().size();++i) {
-            children[i] = treeRuleToStringRule(homomorphicImage.getChildren().get(i));
+        
+        Tree<String>[] children = new Tree[2];
+        
+        int pos = 1;
+        for(int i=homomorphicImage.getChildren().size()-1;i>=0;--i) {
+            children[pos--] = treeRuleToStringRule(homomorphicImage.getChildren().get(i));
+            
+            if(pos < 0) {
+                children[1] = Tree.create(StringAlgebra.CONCAT, children);
+                pos = 0;
+            }
         }
         
-        return Tree.create(StringAlgebra.CONCAT, children);
+        return children[1];
     }
 }
---
+++ src/test/java/de/up/ling/irtg/rule_finding/grammar_post/StringifyTest.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.up.ling.irtg.rule_finding.grammar_post;
+
+import de.up.ling.irtg.InterpretedTreeAutomaton;
+import de.up.ling.irtg.algebra.ParserException;
+import de.up.ling.irtg.automata.TreeAutomaton;
+import de.up.ling.irtg.codec.IrtgInputCodec;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author teichmann
+ */
+public class StringifyTest {
+    /**
+     * 
+     */
+    public static String GRAMMAR = "/* Example tree-to-string transducer from Galley et al. 04 */\n"
+            + "\n"
+            + "interpretation french: de.up.ling.irtg.algebra.StringAlgebra\n"
+            + "interpretation english: de.up.ling.irtg.algebra.TreeAlgebra\n"
+            + "\n"
+            + "S! -> r1(NP,VB)\n"
+            + "[french]  *(*(*(?1, ne), ?2), pas)\n"
+            + "[english] s(?1, vp(aux(does), rb(not), ?2))\n"
+            + "\n"
+            + "NP -> r2\n"
+            + "[french]  il\n"
+            + "[english] np(prp(he))\n"
+            + "\n"
+            + "VB -> r3\n"
+            + "[french]  va\n"
+            + "[english] vb(go)";
+
+    /**
+     * 
+     */
+    private InterpretedTreeAutomaton ita;
+    
+    @Before
+    public void setUp() throws IOException {
+        IrtgInputCodec iic = new IrtgInputCodec();
+        
+        ita = iic.read(new ByteArrayInputStream(GRAMMAR.getBytes()));
+    }
+
+    /**
+     * Test of addStringInterpretation method, of class Stringify.
+     * @throws de.up.ling.irtg.algebra.ParserException
+     */
+    @Test
+    public void testAddStringInterpretation() throws ParserException {
+        Stringify.addStringInterpretation(ita, "english", "engString");
+        
+        Map<String,String> m = new HashMap<>();
+        m.put("engString", "he does not go");
+        
+        TreeAutomaton ta = ita.parse(m);
+        
+        System.out.println(ita.interpret(ta.viterbi(), "french"));
+        assertEquals(ita.interpret(ta.viterbi(), "french").toString(),"[il, ne, va, pas]");
+    }
+}
