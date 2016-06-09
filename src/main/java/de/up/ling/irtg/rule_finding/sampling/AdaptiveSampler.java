ce5722049dd318cec224f954a7386f6bb8bf00c9
Christoph Teichmann <christoph.teichmann.ii@uni-potsdam.de>
2016-06-08 17:40:13 -0700

    Removed Stat-tracking and allowed for turning reset off.
    Now we do not expend any time to keep track of statistics. We can also set a reset option in order to keep the Adaptive Sampler
    from resetting the underlying RuleWeighter.

--- src/main/java/de/up/ling/irtg/rule_finding/sampling/AdaptiveSampler.java
+++ src/main/java/de/up/ling/irtg/rule_finding/sampling/AdaptiveSampler.java
@@ -12,37 +12,24 @@ import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.LongSupplier;
-import org.apache.commons.math3.random.RandomGenerator;
-import org.apache.commons.math3.random.Well44497b;
 
 /**
  *
  * @author teichmann
  */
 public class AdaptiveSampler {
-    /**
-     * 
-     */
-    private SamplingStatistics keptStats = null;
-    
-    /**
-     *
-     */
-    private final Proposal prop;
 
     /**
      * 
      */
-    private final RandomGenerator rg;
-    
+    private final Proposal prop;
+
     /**
      *
      * @param seed
      */
     public AdaptiveSampler(long seed) {
-        this.rg = new Well44497b(seed);
-        
-        this.prop = new Proposal(this.rg.nextLong());
+        this.prop = new Proposal(seed);
     }
 
     /**
@@ -54,146 +41,140 @@ public class AdaptiveSampler {
 
     /**
      * 
-     * @return 
-     */
-    public SamplingStatistics getKeptStats() {
-        return keptStats;
-    }
-
-    /**
-     * 
-     * @param keptStats 
-     */
-    public void setKeptStats(SamplingStatistics keptStats) {
-        this.keptStats = keptStats;
-    }
-    
-    /**
-     * 
      * @param rounds
      * @param populationSize
      * @param rw
      * @param deterministic
+     * @param reset
      * @return 
      */
     public List<TreeSample<Rule>> adaSample(int rounds, int populationSize, RuleWeighting rw,
-                                           boolean deterministic) {
+            boolean deterministic, boolean reset) {
         List<TreeSample<Rule>> result = new ArrayList<>();
-        rw.reset();
+
         ProposalSumComputer psc = new ProposalSumComputer();
         
-        for(int i=0;i<rounds;++i) {
-            TreeSample<Rule> sample = this.prop.getTreeSample(rw, populationSize);
-            
-            
-            for(int j=0;j<populationSize;++j) {
+        if(reset) {
+            rw.reset();
+        }
+
+        for (int i = 0; i < rounds; ++i) {
+            TreeSample<Rule> sample = prop.getTreeSample(rw, populationSize);
+
+            for (int j = 0; j < populationSize; ++j) {
                 sample.setLogTargetWeight(j, rw.getLogTargetProbability(sample.getSample(j)));
             }
-            
-            if(!deterministic) {
+
+            if (!deterministic) {
                 psc.reset();
-                
-                for(int j=0;j<populationSize;++j) {
+
+                for (int j = 0; j < populationSize; ++j) {
                     sample.setLogSumWeight(j, psc.computeInside(sample.getSample(j), rw));
                 }
             }
-            
-            if(this.keptStats != null) {
-                this.keptStats.addUnNormalizedRound(i,sample,rw);
-            }
-            
-            rw.adapt(sample,deterministic);
-            
-            if(this.keptStats != null) {
-                this.keptStats.trackAfterAdaption(rounds, sample, rw);
-            }
-            
+
+            rw.adapt(sample, deterministic);
+
             sample.expoNormalize(deterministic);
             result.add(sample);
         }
-        
+
         return result;
     }
-    
+
     /**
-     * 
+     *
      */
     public static class Configuration {
+
         /**
-         * 
+         *
          */
         private boolean deterministic = true;
-        
+
         /**
-         * 
+         *
          */
         private int rounds = 50;
-        
+
         /**
-         * 
+         *
          */
         private int populationSize = 2000;
+
+        /**
+         *
+         */
+        private final Function<TreeAutomaton, RuleWeighting> rwSource;
         
         /**
          * 
          */
-        private final Function<TreeAutomaton,RuleWeighting> rwSource;
-        
+        private boolean reset = false;
+
         /**
-         * 
+         *
          */
         private LongSupplier seeds = () -> new Date().getTime();
 
         /**
-         * 
-         * @param rwSource 
+         *
+         * @param rwSource
          */
         public Configuration(Function<TreeAutomaton, RuleWeighting> rwSource) {
             this.rwSource = rwSource;
         }
-        
+
         /**
-         * 
-         * @param deterministic 
+         *
+         * @param deterministic
          */
         public void setDeterministic(boolean deterministic) {
             this.deterministic = deterministic;
         }
 
         /**
-         * 
-         * @param rounds 
+         *
+         * @param rounds
          */
         public void setRounds(int rounds) {
             this.rounds = rounds;
         }
 
         /**
-         * 
-         * @param populationSize 
+         *
+         * @param populationSize
          */
         public void setPopulationSize(int populationSize) {
             this.populationSize = populationSize;
         }
 
         /**
-         * 
-         * @param seeds 
+         *
+         * @param seeds
          */
         public void setSeeds(LongSupplier seeds) {
             this.seeds = seeds;
         }
+
+        /**
+         * ,
+         * @param reset 
+         */
+        public void setReset(boolean reset) {
+            this.reset = reset;
+        }
         
         /**
-         * 
+         *
          * @param ta
-         * @return 
+         * @return
          */
         public List<TreeSample<Rule>> run(TreeAutomaton ta) {
             RuleWeighting rw = this.rwSource.apply(ta);
             AdaptiveSampler adas = new AdaptiveSampler(this.seeds.getAsLong());
-            
-            return adas.adaSample(rounds, populationSize, rw, deterministic);
+
+            return adas.adaSample(rounds, populationSize, rw, deterministic, reset);
         }
     }
 }
