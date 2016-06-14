9327096ab8a3f08e08c5255abea9f937b3176aa2
Christoph Teichmann <christoph.teichmann.ii@uni-potsdam.de>
2016-06-14 16:45:25 -0700

    IntTupleIterator tested and bug fixed.
    This creates actual test cases for IntTupleIterator and removes a bug in the way that Emptyness is established.

--- src/test/java/de/up/ling/irtg/util/IntTupleIteratorTest.java
+++ src/test/java/de/up/ling/irtg/util/IntTupleIteratorTest.java
@@ -5,6 +5,12 @@
  */
 package de.up.ling.irtg.util;
 
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntIterable;
+import it.unimi.dsi.fastutil.ints.IntList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -14,12 +20,46 @@ import static org.junit.Assert.*;
  * @author teichmann
  */
 public class IntTupleIteratorTest {
+    /**
+     * 
+     */
+    private IntTupleIterator iti1;
     
-    public IntTupleIteratorTest() {
-    }
+    /**
+     * 
+     */
+    private IntTupleIterator iti2;
+    
+    /**
+     * 
+     */
+    private IntTupleIterator iti3;
+    
+    
     
     @Before
     public void setUp() {
+        IntList il = new IntArrayList();
+        il.add(1);
+        il.add(2);
+        
+        IntList empty = new IntArrayList();
+        
+        List<IntIterable> container = new ArrayList<>();
+        container.add(il);
+        container.add(il);
+        
+        iti1 = new IntTupleIterator(container);
+        
+        container = new ArrayList<>();
+        
+        iti2 = new IntTupleIterator(container);
+        
+        container = new ArrayList<>();
+        container.add(il);
+        container.add(empty);
+        
+        iti3 = new IntTupleIterator(container);
     }
 
     /**
@@ -27,7 +67,15 @@ public class IntTupleIteratorTest {
      */
     @Test
     public void testHasNext() {
-        // TODO review the generated test code and remove the default call to fail.
+        assertFalse(iti2.hasNext());
+        assertFalse(iti3.hasNext());
+        
+        for(int i=0;i<4;++i) {
+            assertTrue(iti1.hasNext());
+            iti1.next();
+        }
+        
+        assertFalse(iti1.hasNext());
     }
 
     /**
@@ -35,7 +83,10 @@ public class IntTupleIteratorTest {
      */
     @Test
     public void testNext() {
-        // TODO review the generated test code and remove the default call to fail.
+        assertArrayEquals(new int[] {1,1}, iti1.next());
+        assertArrayEquals(new int[] {1,2}, iti1.next());
+        assertArrayEquals(new int[] {2,1}, iti1.next());
+        assertArrayEquals(new int[] {2,2}, iti1.next());
     }
     
 }
