6fdbfb135a5fe27dbcb584e2744b119b6c983426
Jonas Groschwitz <jonas.groschwitz@students.mq.edu.au>
2017-01-16 13:15:25 +1100

    added untracked source file

---
+++ src/main/java/de/up/ling/irtg/automata/Intersectable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.up.ling.irtg.automata;
+
+/**
+ * Dummy interface that both TreeAutomaton and SiblingFinderInvhom implement.
+ * @author Jonas
+ */
+public interface Intersectable<State> {
+    
+}
