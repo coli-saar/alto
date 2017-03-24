52b974e33aa38b8766fa123da51b5a2e241592bc
cteichmann <cteichmann@localhost>
2017-03-24 14:41:39 +0100

    added some comments in the util package

--- src/main/java/de/up/ling/irtg/util/MutableInteger.java
+++ src/main/java/de/up/ling/irtg/util/MutableInteger.java
@@ -7,31 +7,57 @@
 package de.up.ling.irtg.util;
 
 /**
- *
+ * This implements a mutable version of an integer.
+ * 
+ * This is essentially an AtomicInteger, without any synchronization effort.
+ * 
  * @author koller
  */
 public class MutableInteger {
-    private int value;
+    private int value = 0;
 
+    /**
+     * Creates a new instance with value equal to 0.
+     */
     public MutableInteger() {
     }
 
+    /**
+     * Creates a new instance with value equal to what is given.
+     * @param value 
+     */
     public MutableInteger(int value) {
         this.value = value;
     }
 
+    /**
+     * Returns the current value.
+     * @return 
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Changes the value to one given.
+     * @param value 
+     */
     public void setValue(int value) {
         this.value = value;
     }
     
+    /**
+     * Increases the value by 1 and returns the old value.
+     * @return 
+     */
     public int incValue() {
         return value++;
     }
     
+    /**
+     * Returns the maximum of the current value and the one given.
+     * @param value 
+     */
     public void max(int value) {
         this.value = Math.max(value, this.value);
     }
@@ -41,6 +67,15 @@ public class MutableInteger {
         return Integer.toString(value);
     }
     
+    /**
+     * Creates a string by incrementing the value and returning the concatenation
+     * of the prefix and the old value.
+     * 
+     * This is used to generate sequences of unique symbols.
+     * 
+     * @param prefix
+     * @return 
+     */
     public String gensym(String prefix) {
         return prefix + incValue();
     }
