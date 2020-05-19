0420a785c4a927486b81407c2e0d3de29863683a
Jonas Groschwitz <jonas.groschwitz@coli.uni-saarland.de>
2020-05-19 08:54:03 +0200

    even more EM stability

--- src/main/java/de/up/ling/irtg/semiring/LogDoubleArithmeticSemiring.java
+++ src/main/java/de/up/ling/irtg/semiring/LogDoubleArithmeticSemiring.java
@@ -14,7 +14,17 @@ public class LogDoubleArithmeticSemiring implements Semiring<Double> {
         if (x == infinity() || y == infinity()) {
             return infinity();
         } else {
-            return Math.log(Math.exp(x) + Math.exp(y));
+            //return Math.log(Math.exp(x) + Math.exp(y));
+            // the below computes the commented out above, but with more numerical stability
+            // c.f. https://gasstationwithoutpumps.wordpress.com/2014/05/06/sum-of-probabilities-in-log-prob-space/
+            if (x > y) {
+                return x + Math.log1p(Math.exp(y-x));
+            } else if (y > x) {
+                return y + Math.log1p(Math.exp(x-y));
+            } else {
+                // x == y
+                return y + Math.log1p(1);
+            }
         }
     }
 
