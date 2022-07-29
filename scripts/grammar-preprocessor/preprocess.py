bb786e3834154fb49f455feb70aef912b426bd0a
Alexander Koller <akoller@gmail.com>
2022-07-29 07:58:08 +0200

    oops

--- scripts/grammar-preprocessor/preprocess.py
+++ scripts/grammar-preprocessor/preprocess.py
@@ -1,4 +1,5 @@
 from jinja2 import *
+import sys
 
 class _Counter(object):
   def __init__(self, start_value=1):
@@ -15,7 +16,7 @@ class _Counter(object):
 env = Environment(loader=FileSystemLoader("."))
 env.globals['counter'] = _Counter
 
-template = env.get_template("main.irtg")
+template = env.get_template(sys.argv[1])
 print(template.render())
 
 
