4649687d8df5615357a103a7b52354dba7401f80
Alexander Koller <akoller@gmail.com>
2022-08-02 08:26:19 +0200

    can now compute PP/CP embedding depth

--- src/main/java/de/up/ling/irtg/script/CogsCorpusGenerator.java
+++ src/main/java/de/up/ling/irtg/script/CogsCorpusGenerator.java
@@ -2,6 +2,7 @@ package de.up.ling.irtg.script;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.google.common.primitives.Ints;
 import de.saar.basic.StringTools;
 import de.saar.coli.algebra.OrderedFeatureTreeAlgebra;
 import de.up.ling.irtg.Interpretation;
@@ -45,13 +46,59 @@ public class CogsCorpusGenerator {
             // If sentence uses the same noun twice, skip it.
             List<String> nouns = collectNouns(ruleTree, irtg.getAutomaton(), irtg.getInterpretation("english").getHomomorphism());
             if( nouns.size() != new HashSet<String>(nouns).size() ) {
+                i--;
                 continue;
             }
+
+            Tree<String> nonterminalTree = getNonterminalTree(ruleTree, irtg.getAutomaton());
+            System.out.printf("PP depth: %d\n", getEmbeddingDepth(nonterminalTree, "PP_"));
+            System.out.printf("CP depth: %d\n", getEmbeddingDepth(nonterminalTree, "S")-1);
+
 
             // TODO Filter the rule trees based on PP/CP embedding depth etc.
 
             System.out.printf("%s\t%s\n", english, ft.toString(true));
         }
+    }
+
+    /**
+     * Returns the number of occurrences of a nonterminal in a path of the given tree.
+     * More specifically, the method determines the number of nodes on each path of the tree
+     * whose labels start with "prefix", and returns the maximum count over all paths.
+     *
+     * @param nonterminalTree
+     * @param prefix
+     * @return
+     */
+    private static int getEmbeddingDepth(Tree<String> nonterminalTree, String prefix) {
+        return nonterminalTree.dfs((node, childMaxDepths) -> {
+            if( childMaxDepths.isEmpty() ) {
+                return 0;
+            } else {
+                int maxChildDepth = max(childMaxDepths);
+                if (node.getLabel().startsWith(prefix)) {
+                    return maxChildDepth + 1;
+                } else {
+                    return maxChildDepth;
+                }
+            }
+        });
+    }
+
+    private static int max(List<Integer> ints) {
+        int ret = Integer.MIN_VALUE;
+        for( int x : ints ) {
+            ret = Integer.max(ret, x);
+        }
+        return ret;
+    }
+
+    private static Tree<String> getNonterminalTree(Tree<Rule> ruleTree, TreeAutomaton<String> auto) {
+        return ruleTree.dfs((node, children) -> {
+            Rule rule = node.getLabel();
+            String lhs = auto.getStateForId(rule.getParent());
+            return Tree.create(lhs, children);
+        });
     }
 
     private static List<String> collectNouns(Tree<Rule> ruleTree, TreeAutomaton<String> auto, Homomorphism stringHomomorphism) {
